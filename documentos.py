f01ed084f7c61bfef9b4a6a2b3db93df94bd5009
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2023-02-23 11:25:28 -0300

    Introducao Collection

---
+++ documentos.py
@@ -0,0 +1,50 @@
+from validate_docbr import CPF,CNPJ
+
+class Documento:
+
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
+        else:
+            raise ValueError('Documento inválido')
+
+class DocCpf:
+    def __init__(self,documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError('CPF inválido')
+
+    def __str__(self):
+        print('')
+        return self.format()
+
+    def valida(self,documento):
+        validator = CPF()
+        return validator.validate(documento)
+
+    def format(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+
+class DocCnpj:
+    def __init__(self,documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError('CNPJ inválido')
+
+    def __str__(self):
+        print('')
+        return self.format()
+
+    def valida(self, documento):
+        mascara = CNPJ()
+        return mascara.validate(documento)
+
+    def format(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
---
+++ padroesRegulares.py
@@ -0,0 +1,20 @@
+import re
+
+padrao = '[0-9][a-z][0-9]'
+texto = '123 1a2 1cc aal'
+resposta = re.search(padrao,texto)
+print(resposta.group())
+
+padrao = "[0-9][a-z]{2}[0-9]"
+texto = "123 1ac2 1cc aa1"
+resposta = re.search(padrao, texto)
+print(resposta.group())
+
+padrao = '\w{5,50}@\w{3,10}.\w{2,3}'
+texto = 'aaabbbcc clauciofds@gamil.com.br ccbbbbaaa'
+resposta = re.search(padrao, texto)
+print(resposta.group())
+
+padrao = '\w{5,50}@[a-z]{2,10}.com.br'
+resposta = re.search(padrao, texto)
+print(resposta.group())
