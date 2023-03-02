f42a4497e26c2c440b3ccdd90cbf5e304526637a
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2023-03-02 13:27:19 -0300

    PROJETO FINALIZADO

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import requests
+
 from Telefones import TelefoneBr
 from datasb_br import DataBr
+from acesso_cep import BuscaEndereco
 import re
 
 
@@ -14,3 +17,19 @@ print(cadastro.format_data())
 
 hoje = DataBr()
 print(hoje.tempo_cadastro())
+
+cep = "13843204"
+objeto_cep = BuscaEndereco(cep)
+
+
+r = requests.get('https://viacep.com.br/ws/13843204/json/')
+print(r.text)
+
+a = objeto_cep.acessa_via_cep()
+# print(type(a.text))
+# print(type(a.json()))
+# print(a.json()['cep'])
+# print(a.json()['bairro'])
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro,cidade,uf)
